d70a185953e1ad761ae9d854e91666ae42c6f4d9
Christoph Lipautz <christoph@lipautz.org>
2016-04-27 20:32:10 +0200

    add a user to consume events
    yes... it's user, not user_id although it is an integer. we have a
    numpad to differ between users for now and it's still a prototype...

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416164211) do
+ActiveRecord::Schema.define(version: 20160427183025) do
 
   create_table "consume_events", force: :cascade do |t|
     t.integer  "analog_reading"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160416164211) do
     t.datetime "consumed_at"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "user"
   end
 
 end
