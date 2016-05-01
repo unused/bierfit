2582f7423721c4f602445b315dbcb03dff467985
Christoph Lipautz <christoph@lipautz.org>
2016-05-01 15:12:39 +0200

    introduce a dashboar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # user has to be signed in, TODO protect json requests(!)
+  before_action :authenticate_user!, unless: :json_request?
+
   skip_before_action :verify_authenticity_token, if: :json_request?
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
+  get 'dashboard/index'
+
   resources :consume_events, only: ['index', 'create']
-  root 'consume_events#index'
+
+  root 'dashboard#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
