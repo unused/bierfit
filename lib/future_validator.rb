2d276c90f28f19c09bec246b8cfb087df28121f0
Christoph Lipautz <christoph@lipautz.org>
2016-05-06 16:54:59 +0200

    ensure guy is drinking

---
+++ lib/future_validator.rb
@@ -0,0 +1,8 @@
+
+class FutureValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    if value && value > Time.now 
+      record.errors[attribute] << (options[:message] || "can't be in the past!")
+    end
+  end
+end
