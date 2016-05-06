9b242880a9259292a15c4b40693696eceded23ca
Christoph Lipautz <christoph@lipautz.org>
2016-05-06 17:00:51 +0200

    rubocop does not like trailing spaces

--- lib/future_validator.rb
+++ lib/future_validator.rb
@@ -1,7 +1,7 @@
 
 class FutureValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if value && value > Time.now 
+    if value && value > Time.now
       record.errors[attribute] << (options[:message] || "can't be in the past!")
     end
   end
