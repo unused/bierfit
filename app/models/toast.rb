42fc07edc07a52e6f6c2ad5633557383bfc7c810
Christoph Lipautz <christoph@lipautz.org>
2016-05-21 10:19:47 +0200

    freeze constant array

--- app/models/toast.rb
+++ app/models/toast.rb
@@ -3,7 +3,7 @@ class Toast
   COLLECTION = [
     'prost', 'skål', 'cheers', 'prosit', 'okole maluna', 'salute', 'kampai',
     'noroc', 'budem'
-  ]
+  ].freeze
 
   def self.sample
     COLLECTION.sample
