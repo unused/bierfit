6c6ead84fa748f90c1a3a63b0826ae7142a44e8b
Christoph Lipautz <christoph@lipautz.org>
2016-05-21 10:18:39 +0200

    do some styling

---
+++ app/models/toast.rb
@@ -0,0 +1,11 @@
+
+class Toast
+  COLLECTION = [
+    'prost', 'skål', 'cheers', 'prosit', 'okole maluna', 'salute', 'kampai',
+    'noroc', 'budem'
+  ]
+
+  def self.sample
+    COLLECTION.sample
+  end
+end
