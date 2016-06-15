cb9231915a4e684165d8423a45bf29ccdf7e2451
Christoph Lipautz <christoph@lipautz.org>
2016-06-15 19:49:46 +0200

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614223159) do
-
-  create_table "admin_users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20160615060755) do
 
   create_table "beers", force: :cascade do |t|
     t.integer  "amount_in_ml"
@@ -36,10 +31,10 @@ ActiveRecord::Schema.define(version: 20160614223159) do
     t.integer  "conductance_in_micromhos"
     t.integer  "force_in_newtons"
     t.datetime "consumed_at"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
     t.integer  "user_id"
-    t.integer  "user"
+    t.boolean  "processed",                default: false, null: false
   end
 
   add_index "consume_events", ["user_id"], name: "index_consume_events_on_user_id"
@@ -74,22 +69,22 @@ ActiveRecord::Schema.define(version: 20160614223159) do
   add_index "gulps", ["beer_id"], name: "index_gulps_on_beer_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.boolean  "admin"
+    t.boolean  "admin",                  default: false, null: false
     t.string   "username"
     t.string   "name"
     t.boolean  "public"
