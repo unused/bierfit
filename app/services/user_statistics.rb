86fe82eb5dacc8c40410eb1d14031d693073d731
Christoph Lipautz <christoph@lipautz.org>
2016-06-18 18:14:02 +0200

    fix rubocop

--- app/services/user_statistics.rb
+++ app/services/user_statistics.rb
@@ -1,5 +1,14 @@
 
 class UserStatistics
+  DRINKER_LEVEL = [
+    'a member of Bierdonnerstag',
+    'a citizen of Bierbardos',
+    'a Bierat',
+    'a visitor of Bierbardos',
+    'a tourist on the Bierberg',
+    'an intern'
+  ].freeze
+
   def initialize(user)
     @user = user
   end
@@ -37,49 +46,40 @@ class UserStatistics
   end
 
   def drinker_in_words
-    case (total_max_beer_per_day - max_beer_per_day)
-      when 0 then 'a member of Bierdonnerstag'
-      when 1 then 'a citizen of Bierbardos'
-      when 2 then 'a Bierat'
-      when 3 then 'a visitor of Bierbardos'
-      when 4 then 'a tourist on the Bierberg'
-      when 5 then 'an intern'
-      else 'a beginner'
-    end
+    DRINKER_LEVEL[total_max_beer_per_day - max_beer_per_day] || 'a beginner'
   end
 
   def bierfit_position_in_percentage
     user_positions = Beer.select('count(*) as cnt, user_id')
-      .group(:user_id).order('cnt ASC')
+                         .group(:user_id).order('cnt ASC')
     user_position  = user_positions.map(&:user_id).index(@user.id)
-    user_position.to_f / user_positions.to_a.count * 100
+    user_position.to_f / user_positions.to_a.size * 100
   end
 
   def beers
     @user.beers
   end
 
-  # TODO use collected queries to reduce db-requests on demand:
+  # TODO: use collected queries to reduce db-requests on demand:
   private
 
-    def gulps
-      Gulp.joins(:beer).where('beers.user_id = ?', @user.id)
-    end
+  def gulps
+    Gulp.joins(:beer).where('beers.user_id = ?', @user.id)
+  end
 
-    def beer_stats
-      @beer_stats || collect_beer_stats
-    end
+  def beer_stats
+    @beer_stats || collect_beer_stats
+  end
 
-    def collect_beer_stats
-      @beer_stats = Beer.select('...')
-    end
+  def collect_beer_stats
+    @beer_stats = Beer.select('...')
+  end
 
-    def gulp_stats
-      @gulp_stats || collect_gulp_stats
-    end
+  def gulp_stats
+    @gulp_stats || collect_gulp_stats
+  end
 
-    def collect_gulp_stats
-      @gulp_stats = Beer.select('...')
-    end
-
+  def collect_gulp_stats
+    @gulp_stats = Beer.select('...')
+  end
 end
